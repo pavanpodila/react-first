5adbda4cfb9501cfbb7ba871b9020456fcf79f12
pavanpodila <pavan.podila@gmail.com>
2018-03-30 13:19:40 +0530

    test run 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Background } from './core/components/Background';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { LandingPage } from './pages/LandingPage';
+import { ClockPage } from './pages/ClockPage';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+                <Background src={require('./assets/raindrops.jpg')} />
+
+                <BrowserRouter>
+                    <Fragment>
+                        <Header />
+                        <Switch>
+                            <Route path={'/'} exact component={LandingPage} />
+                            <Route
+                                path={'/clock'}
+                                exact
+                                component={ClockPage}
+                            />
+                        </Switch>
+                    </Fragment>
+                </BrowserRouter>
             </div>
         );
     }
+}
+
+function Header() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Link to={'/'}>
+                <h1 className="App-title">Welcome to React</h1>
+            </Link>
+        </header>
+    );
 }
 
 export default App;
